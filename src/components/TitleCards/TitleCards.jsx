58320b74843b9ff15c059f26f4bf3f542e87a41c
Reza-Pourdast <s.rezapourdast@gmail.com>
2025-01-14 20:19:48 +0330

    Update home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './TitleCards.css'
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
+const TitleCards = ({ title, category }) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
   return (
-    <div className='titleCards'>
-      
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} className="card-img" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
-    </div>
-  )
-}
+    <div className="home">
+      <Navbar />
 
-export default Home
+      <div className="hero">
+        <img src={hero_banner} className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} className="caption-img" />
+          <p>
+            "The Guardian" is about a young man who inherits ancient powers and
+            must protect his city from dark forces, embarking on a thrilling
+            adventure of danger and self-discovery.{" "}
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top picks for you"} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
